aac4bd794e1c345709531e85174a9593a8aac3f4
Stephan Hügel <shugel@tcd.ie>
2018-10-24 18:15:13 +0100

    Minify

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,17 @@
 import babel from 'rollup-plugin-babel';
+import uglify from 'rollup-plugin-uglify';
 
 module.exports = {
   input: 'static/script.js',
   plugins: [
     babel({
       exclude: 'node_modules/**'
-    })
+    }),
+    uglify.uglify()
   ],
   output: {
     file: 'static/bundle.js',
-    format: 'cjs'
+    format: 'iife',
+    sourceMap: 'inline',
   }
 };
