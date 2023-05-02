4ce385828879b5bcc0ec6ddf0d2550a81c6a7e61
Stephan Hügel <shugel@tcd.ie>
2023-05-02 17:00:25 +0100

    Add browserslist targets

--- build.mjs
+++ build.mjs
@@ -5,9 +5,17 @@ import autoprefixer from 'autoprefixer';
 
 import eslint from 'esbuild-plugin-eslint';
 
+import browserslist from 'browserslist';
+import {
+  esbuildPluginBrowserslist,
+  resolveToEsbuildTarget,
+} from 'esbuild-plugin-browserslist';
+
 await esbuild.build({
   entryPoints: ['script.js'],
   bundle: true,
+  minify: true,
+  sourcemap: true,
   metafile: true,
   outdir: 'static',
   plugins: [
@@ -17,6 +25,9 @@ await esbuild.build({
               return css;
           },
       }),
-      eslint({ /* config */ })
+      eslint({ /* config */ }),
+      esbuildPluginBrowserslist(browserslist('>0.2%, not dead, not ie 11, not chrome < 51, not safari < 10'), {
+        printUnknownTargets: false,
+      }),
   ],
 })
