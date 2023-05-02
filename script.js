c994cc0a1e69b65c3ba28b9fd96ae1b131ddcd4b
Stephan Hügel <shugel@tcd.ie>
2023-05-02 17:21:09 +0100

    Use Sky

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import mapboxgl from 'mapbox-gl';
+import SunCalc from 'suncalc'
 import 'bootstrap';
 import './style.scss'
 
@@ -58,6 +59,47 @@ const map = new mapboxgl.Map({
     }]
 });
 
+// update the `sky-atmosphere-sun` paint property with the position of the sun based on the selected time
+// the position of the sun is calculated using the SunCalc library
+function updateSunPosition(sunPos) {
+    map.setPaintProperty('sky', 'sky-atmosphere-sun', sunPos);
+}
+
+// Get list of SunCalc's default sun positions
+// for the current time and location
+const sunPositions = SunCalc.getTimes(
+    Date.now(),
+    map.getCenter().lat,
+    map.getCenter().lng
+);
+// set up event listeners for the buttons to update
+// the position of the sun for times of the day
+const sunTimeLabels = document.querySelectorAll(
+    '#inputs input',
+    '#getlocal'
+);
+for (const label of sunTimeLabels) {
+    label.addEventListener('click', () => {
+        const sunPos =
+            label.id === 'getlocal'
+                ? getSunPosition(new Date())
+                : getSunPosition(sunPositions[label.id]);
+        updateSunPosition(sunPos);
+    });
+}
+
+function getSunPosition(date) {
+    const center = map.getCenter();
+    const sunPos = SunCalc.getPosition(
+        date || Date.now(),
+        center.lat,
+        center.lng
+    );
+    const sunAzimuth = 180 + (sunPos.azimuth * 180) / Math.PI;
+    const sunAltitude = 90 - (sunPos.altitude * 180) / Math.PI;
+    return [sunAzimuth, sunAltitude];
+}
+
 map.on('style.load', function() {
     map
         .addSource(
@@ -74,6 +116,44 @@ map.on('style.load', function() {
         bearing: Math.floor(Math.random() * (360 - 1 + 1)) + 1,
         pitch: Math.floor(Math.random() * (70.0 - 1.0 + 1.0)) + 50.0,
     });
+    map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+    });
+    map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+        map.addLayer({
+            'id': 'sky',
+            'type': 'sky',
+            'paint': {
+                'sky-opacity': [
+                    'interpolate',
+                    ['linear'],
+                    ['zoom'],
+                    0,
+                    0,
+                    5,
+                    0.3,
+                    8,
+                    1
+                ],
+                // set up the sky layer for atmospheric scattering
+                'sky-type': 'atmosphere',
+                // explicitly set the position of the sun rather than allowing the sun to be attached to the main light source
+                'sky-atmosphere-sun': getSunPosition(),
+                // set the intensity of the sun as a light source (0-100 with higher values corresponding to brighter skies)
+                'sky-atmosphere-sun-intensity': 5
+            }
+        });
+    // });
+    // map.setFog({
+    //         color: 'rgb(186, 210, 235)', // Lower atmosphere
+    //         'high-color': 'rgb(36, 92, 223)', // Upper atmosphere
+    //         'horizon-blend': 0.02, // Atmosphere thickness (default 0.2 at low zooms)
+    //         'space-color': 'rgb(11, 11, 25)', // Background color
+    //         'star-intensity': 0.6 // Background star brightness (default 0.35 at low zoooms )
+    // });
 });
 
 map.on('load', () => {
